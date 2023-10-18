0cd4bffe3445db19088ee50ce72021ddf83e2deb
artem_emelianenko <akk_12@mail.ru>
2023-10-18 14:57:30 +0300

    add: role in view & controller

--- resources/views/email/confirmed.blade.php
+++
@@ -1,6 +0,0 @@
-@component('mail::message')
-    Артикуль продукта: {{ $product['article'] }} <br/>
-    Название продукта: {{ $product['name'] }} <br/>
-    Thanks,<br>
-    {{ config('app.name') }}
-@endcomponent
